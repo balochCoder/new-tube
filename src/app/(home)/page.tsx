6a9e1ba9e80e97a7839af67673592c07bd112146
Raheel Ahmed <raheelahmed017@gmail.com>
2025-02-14 19:52:24 +0500

    trpc implementation

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,17 @@
+import {HydrateClient, trpc} from "@/trpc/server";
+import {PageClient} from "@/app/(home)/client";
+import {Suspense} from "react";
+import {ErrorBoundary} from "react-error-boundary";
 
-export default function Home() {
+export default async function Home() {
+   void trpc.hello.prefetch({text: 'Raheel'});
     return (
-        <div>
-            I will load videos in the future
-        </div>
+        <HydrateClient>
+            <Suspense fallback={<p>Loading...</p>}>
+                <ErrorBoundary fallback={<p>Error...</p>}>
+                    <PageClient/>
+                </ErrorBoundary>
+            </Suspense>
+        </HydrateClient>
     );
 }
