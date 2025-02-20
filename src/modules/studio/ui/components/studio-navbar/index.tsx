7fde3dc9e37e362d4fe56c7b7e449c294304e95a
Raheel Ahmed <raheelahmed017@gmail.com>
2025-02-20 17:24:49 +0500

    Studio Protected Route

--- src/modules/studio/ui/components/studio-navbar/index.tsx
+++ src/modules/studio/ui/components/studio-navbar/index.tsx
@@ -2,6 +2,7 @@ import {SidebarTrigger} from "@/components/ui/sidebar";
 import Link from "next/link";
 import Image from "next/image";
 import {AuthButton} from "@/modules/auth/ui/components/auth-button";
+import {StudioUploadModal} from "@/modules/studio/ui/components/studio-upload-modal";
 
 export const StudioNavbar = () => {
     return (
@@ -22,6 +23,7 @@ export const StudioNavbar = () => {
                 <div className="flex-1"/>
 
                 <div className="flex-shrink-0 items-center flex gap-4">
+                    <StudioUploadModal/>
                     <AuthButton/>
                 </div>
             </div>
